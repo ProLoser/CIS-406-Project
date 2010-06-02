db13a1b7c0f4fb8ceb08a34b962e464458a5476c
Dean Sofer <proloser@hotmail.com>
2010-06-02 04:25:10 -0700

    Added a dateutils function for a formatted date

--- src/cis406/DateUtils.java
+++ src/cis406/DateUtils.java
@@ -2,20 +2,28 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package cis406;
+
 import java.util.Calendar;
 import java.text.SimpleDateFormat;
+
 /**
  *
  * @author Mark Lenser
  */
 public class DateUtils {
+
     public static final String DATE_FORMAT_NOW = "yyyyMMdd";
 
     public static String now() {
-    Calendar cal = Calendar.getInstance();
-    SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_NOW);
-    return sdf.format(cal.getTime());
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_NOW);
+        return sdf.format(cal.getTime());
+    }
+
+    public static String nowFormatted() {
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-M-d");
+        return sdf.format(cal.getTime());
     }
 }
